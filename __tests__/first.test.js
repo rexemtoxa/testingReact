a561391ca3eb254bd880678ce5e42251dd2b61db
anton <rehemtoha@gmail.com>
2019-02-24 15:13:55 +0200

    add page object pattern

--- __tests__/first.test.js
+++ __tests__/first.test.js
@@ -2,20 +2,36 @@ import React from 'react';
 import { mount } from 'enzyme';
 import App from '../src/component/App';
 
+
+const selectors = {
+  tabsBox: 'div[data-test="tabBox"]',
+  addTabBtn: 'button[data-test="addTab"]',
+  removeTabBtn: 'button[data-test="tabRemove"]',
+  tabs: 'li[data-test="tab-anchor"]',
+};
+
+const getPage = wrapper => ({
+  getTabsBox: () => wrapper.find(selectors.tabsBox),
+  getTabsNth: index => wrapper.find(selectors.tabs).at(index),
+  getAddTabBtn: () => wrapper.find(selectors.addTabBtn),
+  getRemoveTabBtn: () => wrapper.find(selectors.removeTabBtn),
+});
+
 test('snapshot testing to toggle tab', () => {
   const wrapper = mount(<App />);
   expect(wrapper.render()).toMatchSnapshot();
-  const tabs = wrapper.find('[data-test="tab-anchor"]');
-  const tab2 = tabs.at(2);
+
+  const page = getPage(wrapper);
+  const tab2 = page.getTabsNth(1);
   tab2.simulate('click');
   expect(wrapper.render()).toMatchSnapshot();
 });
 
 test('toggle tab', () => {
   const wrapper = mount(<App />);
-  const tabs = wrapper.find('li[data-test="tab-anchor"]');
-  const firstTab = tabs.at(0);
-  const secondTab = tabs.at(1);
+  const page = getPage(wrapper);
+  const firstTab = page.getTabsNth(0);
+  const secondTab = page.getTabsNth(1);
 
   expect(firstTab).toHaveClassName('react-tabs__tab react-tabs__tab--selected');
   expect(secondTab).not.toHaveClassName('react-tabs__tab--selected');
@@ -23,10 +39,11 @@ test('toggle tab', () => {
 
 test('toggle tab 2', () => {
   const wrapper = mount(<App />);
-  wrapper.find('li[data-test="tab-anchor"]').at(1).simulate('click');
-  const tabs = wrapper.find('li[data-test="tab-anchor"]');
-  const firstTab = tabs.at(0);
-  const secondTab = tabs.at(1);
+  const page = getPage(wrapper);
+  page.getTabsNth(1).simulate('click');
+  const firstTab = page.getTabsNth(0);
+  const secondTab = page.getTabsNth(1);
+  secondTab.simulate('click');
 
   expect(secondTab).toHaveClassName('react-tabs__tab react-tabs__tab--selected');
   expect(firstTab).not.toHaveClassName('react-tabs__tab--selected');
@@ -34,24 +51,24 @@ test('toggle tab 2', () => {
 
 test('add new rab', () => {
   const wrapper = mount(<App />);
-  const tabsBeforeUpdate = wrapper.find('div[data-test="tabBox"]');
-  expect(tabsBeforeUpdate).toContainMatchingElements(5, 'li[data-test="tab-anchor"]');
+  const page = getPage(wrapper);
+  const tabsBeforeUpdate = page.getTabsBox();
+  expect(tabsBeforeUpdate).toContainMatchingElements(5, selectors.tabs);
 
-  const buttonAdd = wrapper.find('button[data-test="addTab"]');
+  const buttonAdd = page.getAddTabBtn();
   buttonAdd.last().simulate('submit');
-  const tabsAfterUpdate = wrapper.find('[data-test="tabBox"]');
-  expect(tabsAfterUpdate).toContainMatchingElements(6, 'li[data-test="tab-anchor"]');
+  const tabsAfterUpdate = page.getTabsBox();
+  expect(tabsAfterUpdate).toContainMatchingElements(6, selectors.tabs);
 });
 
 it('remove tab', () => {
   const wrapper = mount(<App />);
-  const tabsBeforeUpdate = wrapper.find('[data-test="tabBox"]');
+  const page = getPage(wrapper);
+  const tabsBeforeUpdate = page.getTabsBox();
+  expect(tabsBeforeUpdate).toContainMatchingElements(5, selectors.tabs);
 
-  expect(tabsBeforeUpdate).toContainMatchingElements(5, 'li[data-test="tab-anchor"]');
-
-  const tabRemoveButtons = tabsBeforeUpdate.find('button[data-test="tabRemove"]');
+  const tabRemoveButtons = page.getRemoveTabBtn();
   tabRemoveButtons.last().simulate('click');
-  const tabsAfterUpdate = wrapper.find('[data-test="tabBox"]');
-
-  expect(tabsAfterUpdate).toContainMatchingElements(4, 'li[data-test="tab-anchor"]');
+  const tabsAfterUpdate = page.getTabsBox();
+  expect(tabsAfterUpdate).toContainMatchingElements(4, selectors.tabs);
 });
